2f9c6d95d7eb2bca4cdc1311e26d30239e8ded45
Aditya kamble <kambleaditya490@gmail.com>
2025-02-17 10:55:07 +0530

    update backend

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const aiRoutes = require('./routes/ai.routes')
 const cors = require('cors')
+require('dotenv').config()
 
 const app = express()
 
@@ -9,10 +10,21 @@ app.use(cors())
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+// app.get('/', (req, res) => {
+//     res.send('Hello World')
+// })
+
 
 app.use('/ai', aiRoutes)
+
+const PORT = 3000 || process.env.PORT
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`)
+})
+
+
 
 module.exports = app
